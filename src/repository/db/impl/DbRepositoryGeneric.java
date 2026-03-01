94874d27ccce478035b29f4ba78cfd29ef102cc2
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2026-03-01 16:30:03 +0100

    feat: add LEFT JOINs for Dogadjaj and Ucesce in getAll method to include related data

--- src/repository/db/impl/DbRepositoryGeneric.java
+++ src/repository/db/impl/DbRepositoryGeneric.java
@@ -2,6 +2,8 @@ package repository.db.impl;
 
 import domen.ApstraktniDomenskiObjekat;
 import domen.Ansambl;
+import domen.Dogadjaj;
+import domen.Ucesce;
 import java.util.ArrayList;
 import java.util.List;
 import repository.db.DbConnectionFactory;
@@ -19,6 +21,20 @@ public class DbRepositoryGeneric implements DbRepository<ApstraktniDomenskiObjek
         // Za Ansambl, dodaj LEFT JOIN sa zanr tabelom
         if (param instanceof Ansambl) {
             upit = "SELECT ansambl.*, zanr.naziv as zanrNaziv FROM ansambl LEFT JOIN zanr ON ansambl.zanr = zanr.zanrID";
+        }
+        // Za Dogadjaj, dodaj LEFT JOIN sa mesto i ansambl tabelom
+        else if (param instanceof Dogadjaj) {
+            upit = "SELECT dogadjaj.*, mesto.naziv as mestoNaziv, ansambl.imeAnsambla FROM dogadjaj " +
+                   "LEFT JOIN mesto ON dogadjaj.mesto = mesto.mestoID " +
+                   "LEFT JOIN ansambl ON dogadjaj.ansambl = ansambl.ansamblID";
+        }
+        // Za Ucesce, dodaj LEFT JOIN sa clan, ansambl i uloga tabelom
+        else if (param instanceof Ucesce) {
+            upit = "SELECT ucesce.clan, ucesce.ansambl, ucesce.uloga as ulogaID, clandrustva.clanIme, ansambl.imeAnsambla, uloga.naziv as ulogaNaziv " +
+                   "FROM ucesce " +
+                   "LEFT JOIN clandrustva ON ucesce.clan = clandrustva.clanID " +
+                   "LEFT JOIN ansambl ON ucesce.ansambl = ansambl.ansamblID " +
+                   "LEFT JOIN uloga ON ucesce.uloga = uloga.ulogaID";
         }
         
         if (uslov != null) {
